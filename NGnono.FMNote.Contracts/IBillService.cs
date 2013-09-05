995bef9329a1b262bec7653eeff6515d8f5213b9
ngnono <ngnono@126.com>
2013-09-05 18:29:59 +0800

    edit

---
+++ NGnono.FMNote.Contracts/IBillService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NGnono.FMNote.Contracts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBillService
+    {
+        /// <summary>
+        ///  添加
+        /// </summary>
+        /// <param name="model">Bill model</param>
+        /// <returns></returns>
+        dynamic Insert(dynamic model);
+    }
+}
---
+++ NGnono.FMNote.Services/BillService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NGnono.FMNote.Contracts;
+using NGnono.FMNote.Datas.Models;
+using NGnono.FMNote.Repository;
+using NGnono.Framework.Mapping;
+
+namespace NGnono.FMNote.Services
+{
+    public class BillService : IBillService, IDisposable
+    {
+        private readonly FMNoteUnitOfWork _unitOfWork;
+
+        public BillService(FMNoteUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public dynamic Insert(dynamic model)
+        {
+            return _unitOfWork.BillRepository.Insert(Mapper.Map<BillEntity, dynamic>(model));
+        }
+
+        public void Dispose()
+        {
+            _unitOfWork.Dispose();
+        }
+    }
+}
